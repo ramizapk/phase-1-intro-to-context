ea41df4fbcbf0f86585e3667742abe361924779e
webster <wbian618@gmail.com>
2023-01-21 20:43:11 -0500

    commit tree 467e161f2551d4031524eca6f773e73a6d69d294

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function createEmployeeRecord(array) {
+    const employeeObject = {
+      firstName: array[0],
+      familyName: array[1],
+      title: array[2],
+      payPerHour: array[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+    return employeeObject
+  };
+
+function createEmployeeRecords(array2) {
+    const map1 = array2.map(a => createEmployeeRecord(a));
+    return map1
+}
